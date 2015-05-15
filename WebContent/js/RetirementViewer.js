8e905ff38dc020e14b47cb0aff84fb0e49c96ae8
fcadatastrategy <dan.jackson@fca.org.uk>
2015-05-15 17:10:09 +0100

    Viewer2

--- WebContent/js/RetirementViewer.js
+++ WebContent/js/RetirementViewer.js
@@ -1,6 +1,6 @@
 var ProviderApp = angular.module('ViewerApp', ['acute.select']);  // note you can add multiple injectors ['ui.grid','blah']
 
-ProviderApp.controller('ViewerCtrl',function ($scope, $http,uiGridConstants) {
+ProviderApp.controller('ViewerCtrl',function ($scope, $http) {
 	
 	 
 	 $scope.columns =[{ field: 'provider', cellClass: 'TableCell', headerCellClass: 'TableHeader', displayName: 'Provider', enableFiltering:false},
@@ -10,40 +10,43 @@ ProviderApp.controller('ViewerCtrl',function ($scope, $http,uiGridConstants) {
 	// Product Types
 	 $scope.prodtypes = [{"prodtype":"SIPP","id":1},{"prodtype":"PPP","id":2},{"prodtype":"Stakeholder","id":3}];
 	 $scope.selectedProdtype = $scope.prodtypes[0];
-		
+
+	// Return all states when dropdown first opens
+	 $scope.getProdTypes = function (callback) {
+	     callback($scope.prodtypes);
+	 };
+	 
 	// Providers
 	 $scope.providers = [{"provider":"Aviva","id":1},{"provider":"Friends Provident","id":2},{"provider":"Cofunds","id":3}];
 	 $scope.selectedProvider = $scope.providers[0];
+	 
+		// Return all states when dropdown first opens
+	 $scope.getProviders = function (callback) {
+	     callback($scope.providers);
+	 };	 
 	 
 	// Products
 	 $scope.products = [{"product":"Aviva SIPP","id":1},{"product":"Product 2","id":2},{"product":"Product 3","id":3}];
 	 $scope.selectedProduct = $scope.products[0]; 
 	 
+	 $scope.getProducts = function (callback) {
+	     callback($scope.products);
+	 };	 
+	
+	// Data Dates 
 	$http.get('/FCARest/track/prodtracker/retdatadates').success(function(data)  {
-			$scope.datadates = data; //
-			$scope.selectedDate = $scope.datadates[0];
+			$scope.datadates = data; //			 
+
+
 	});
-		    
-	$scope.loadProdProvidersFromServer = function(selectedNode) {
-			//alert($scope.selectedDate.datadate);
-			// Extract the relevant node name from the HTML string in the node.
-			$scope.prodType = selectedNode.substring(selectedNode.search('<td class="nodetab">')+20, selectedNode.search("</br>"));
-			$scope.prodType = $scope.prodType.replace(/\s+/g, '');
-			var callstring = $scope.prodType + "--" + $scope.selectedDate.datadate; 
-			
-			var url = "/FCARest/track/prodtracker/prodprovider/" + callstring;
-			
-			$http.get(url).success(function(data)  {							
-				$scope.gridOptions.data = data;				
-			});	
-			
-			 $scope.hideGrid = {
-					  grid: true
-					};
-			
-	  };	    
-	  
-	  
+	 
+	 //$scope.datadates1 = [{"datadate":"11-05-2015","id":1},{"datadate":"04-05-2015","id":2}];
+	$scope.selectedDate = null;
+	
+/*	 $scope.getDataDates = function (callback) {
+	     callback($scope.datadates);
+	 };	*/
+
 	    
 });	 
 
